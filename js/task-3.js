31442f569ff8521d3e2e7b8c3ef99e263718d2bb
Katerina_Kravets <12d7v38y@gmail.com>
2025-06-16 22:46:00 +0300

    add index.html and add js

--- js/task-3.js
+++ js/task-3.js
@@ -11,7 +11,7 @@ class StringBuilder {
     }
 
     padStart(str) { 
-        this.#value = str + this.#value + str;
+        this.#value = str + this.#value;
     }
     padBoth(str) { 
         this.#value = str + this.#value + str;
